899fc4c0a74b7c4a2c6281dc6b58f155b39ab89d
s4m13337 <ee.sam@protonmail.com>
2023-11-27 20:12:10 +0100

    Adding katex css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'katex/dist/katex.min.css';
 import { Container, Row } from 'react-bootstrap';
 import Sidebar from './sidebar';
 import Canvas from './canvas.js';
